7637ac7cd2c6c897795c998c14a4fe5c2159aa1e
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-07 10:33:56 +0530

    Update colorCoder.js

--- app/colorCoder.js
+++ app/colorCoder.js
@@ -1,4 +1,4 @@
 import {MajorColorNames, MinorColorNames, ColorPair, getColorFromPairNumber, getPairNumberFromColor} from './colorCoderFunctions.js';
-
+import {test} from './test/colorCoderTest.js'
 
 test();
